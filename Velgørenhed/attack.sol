97985ac4df55546cffdd6cad62ff0cdcf65e48c4
Super02 <34713084+Super02@users.noreply.github.com>
2022-05-12 19:55:28 +0200

    Update attack.sol

--- Velgørenhed/attack.sol
+++ Velgørenhed/attack.sol
@@ -6,8 +6,8 @@ contract Attacker {
     Charity public charity;
     bool firstcall = true;
     
-    constructor(address _etherBankAddress) {
-        charity = Charity(_etherBankAddress);
+    constructor(address _charityAddress) {
+        charity = Charity(_charityAddress);
     }
     
     function attack() public payable {
